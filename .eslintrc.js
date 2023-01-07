253a827e36eafb34e11f7eba4ba8f8ad977af9bd
Xeretis <70800211+Xeretis@users.noreply.github.com>
2023-01-07 04:15:32 +0100

    Some fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
     root: true,
+    env: {
+        node: true,
+        commonjs: true,
+    },
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint", "react"],
     extends: [
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,8 +3,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import bcrypt from "bcrypt";
-
-import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db";
 
 export const authOptions: NextAuthOptions = {
@@ -48,15 +46,6 @@ export const authOptions: NextAuthOptions = {
             return user;
         }
     }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
