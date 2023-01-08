d774843428cc6bd05c0217dbe75cf45522a687e3
Xeretis <70800211+Xeretis@users.noreply.github.com>
2023-01-08 08:31:17 +0100

    Made some changes to the home page

--- frontend/MentalWealth/src/components/moodLineChart.tsx
+++ frontend/MentalWealth/src/components/moodLineChart.tsx
@@ -1,40 +1,32 @@
-import { useMantineTheme } from '@mantine/core';
+import { Box, LoadingOverlay, useMantineTheme } from "@mantine/core";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import dayjs from 'dayjs';
-import { Line } from 'react-chartjs-2';
-import { fetcher } from '../utils/fetcher';
-import { useQuery } from '@tanstack/react-query';
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    Title,
+    Tooltip,
+} from "chart.js";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import { Line } from "react-chartjs-2";
+import dayjs from "dayjs";
+import { fetcher } from "../utils/fetcher";
+import { useQuery } from "@tanstack/react-query";
 
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export const MoodChart = () => {
     const theme = useMantineTheme();
 
     function getXGridColors() {
-        theme.colorScheme === "dark" ? theme.colors.gray[9] : theme.colors.gray[3]
+        theme.colorScheme === "dark" ? theme.colors.gray[9] : theme.colors.gray[3];
         let colors = [];
         for (let i = 0; i < 6; i++) {
             colors[i] = theme.colorScheme === "dark" ? theme.colors.gray[9] : theme.colors.gray[3];
         }
-        
+
         colors[6] = theme.fn.primaryColor();
 
         return colors;
@@ -51,7 +43,7 @@ export const MoodChart = () => {
             x: {
                 grid: {
                     color: getXGridColors(),
-                }
+                },
             },
             y: {
                 offset: true,
@@ -63,9 +55,10 @@ export const MoodChart = () => {
                     autoSkip: false,
                 },
                 grid: {
-                    color: theme.colorScheme === "dark" ? theme.colors.gray[9] : theme.colors.gray[3],
-                }
-            }
+                    color:
+                        theme.colorScheme === "dark" ? theme.colors.gray[9] : theme.colors.gray[3],
+                },
+            },
         },
     };
 
@@ -75,48 +68,55 @@ export const MoodChart = () => {
             days[i] = dayjs().subtract(i, "days");
         }
         return days.reverse();
-    }
+    };
 
-    const labels = days().map(day => day.format("dddd"));
+    const labels = days().map((day) => day.format("dddd"));
 
     const chartData = {
-    labels,
-    datasets: [
-        {
-            label: 'Mood',
-            data: [],
-            borderColor: theme.fn.primaryColor(),
-            backgroundColor: theme.fn.primaryColor(),
-            lineTension: 0.25,
-            pointRadius: 4,
-            pointHoverRadius: 8,
-        },
-    ],
+        labels,
+        datasets: [
+            {
+                label: "Mood",
+                data: [],
+                borderColor: theme.fn.primaryColor(),
+                backgroundColor: theme.fn.primaryColor(),
+                lineTension: 0.25,
+                pointRadius: 4,
+                pointHoverRadius: 8,
+            },
+        ],
     };
-    
 
-const { data, status } = useQuery({
+    const entries = useQuery({
         queryKey: ["JournalEntries"],
         queryFn: () => {
             const journalEntries = fetcher.path("/Journals").method("get").create();
             return journalEntries({});
-        }
+        },
     });
 
-    if (status === "success") {
-        for (const day of days()) {
-            const dayAverageMood = data.data.filter(d => dayjs(d.createdAt).isSame(day, "day"))
-                                            .map(d => d.moodLevel)
-                                            .filter(m => m != 0);
+    if (entries.isLoading)
+        return (
+            <Box>
+                <LoadingOverlay visible={true} overlayBlur={2} />
+                <Line options={options} data={chartData} height={100} />
+            </Box>
+        );
 
-            if (dayAverageMood.length > 0) {
-                chartData.datasets[0].data.push(Math.ceil(dayAverageMood.reduce((a, b) => a + b) / dayAverageMood.length));
-            } else {
-                chartData.datasets[0].data.push(undefined);
-            }
+    for (const day of days()) {
+        const dayAverageMood = entries.data.data
+            .filter((d) => dayjs(d.createdAt).isSame(day, "day"))
+            .map((d) => d.moodLevel)
+            .filter((m) => m != 0);
+
+        if (dayAverageMood.length > 0) {
+            chartData.datasets[0].data.push(
+                Math.ceil(dayAverageMood.reduce((a, b) => a + b) / dayAverageMood.length)
+            );
+        } else {
+            chartData.datasets[0].data.push(undefined);
         }
-    } 
+    }
 
-
-    return <Line options={options} data={chartData} height={100} redraw />;  
-}
+    return <Line options={options} data={chartData} height={100} />;
+};
--- frontend/MentalWealth/src/pages/home.tsx
+++ frontend/MentalWealth/src/pages/home.tsx
@@ -1,13 +1,29 @@
-import { Text, Title, createStyles, Loader, Card, SimpleGrid, Center, Group, Space, Anchor, useMantineTheme, Button } from "@mantine/core";
+import {
+    Anchor,
+    Box,
+    Button,
+    Card,
+    Center,
+    Group,
+    Loader,
+    SimpleGrid,
+    Space,
+    Text,
+    Title,
+    createStyles,
+    useMantineTheme,
+} from "@mantine/core";
+
+import { FullScreenLoading } from "../components/fullScreenLoading";
+import { HelperHome } from "../components/helperHomePage";
+import { IconDots } from "@tabler/icons";
+import { Link } from "react-router-dom";
+import { MoodChart } from "../components/moodLineChart";
+import dayjs from "dayjs";
 import { fetcher } from "../utils/fetcher";
+import { hexToRgb } from "../utils/colors";
 import { useApiStore } from "../store/apiStore";
 import { useQuery } from "@tanstack/react-query";
-import { IconDots } from "@tabler/icons";
-import { hexToRgb } from "../utils/colors";
-import dayjs from "dayjs";
-import { MoodChart } from "../components/moodLineChart";
-import { HelperHome } from "../components/helperHomePage";
-import { UserHome } from "../components/userHomePage";
 
 function getHalfOpacityFromHex(hex: string) {
     const [r, g, b] = hexToRgb(hex);
@@ -15,22 +31,125 @@ function getHalfOpacityFromHex(hex: string) {
 }
 
 const useStyles = createStyles((theme) => ({
-    createJournalCard: {
-        borderColor: theme.fn.primaryColor(),
-        color: theme.fn.primaryColor(),
-        boxShadow: theme.colorScheme === "dark" ? "inset 1px 1px 24px 0 " + getHalfOpacityFromHex(theme.fn.primaryColor()) : "",
-
+    errorContainer: {
+        height: "100vh",
+        zIndex: -1,
+        top: 0,
+        left: 0,
+        right: 0,
+        position: "fixed",
+        flexDirection: "column",
     },
 }));
 
 const Home = (): JSX.Element => {
     return (
         <>
-            <Title order={1} mb="xl">Good to see you, {useApiStore((state) => state.user).userName}.</Title>
-            {/* TODO: ROLES */}
-            {useApiStore((state) => state.user).roles.includes("") ? <HelperHome></HelperHome> : <UserHome></UserHome> }
+            <Title order={1} mb="xl">
+                Good to see you, {useApiStore((state) => state.user).userName}.
+            </Title>
+            <SimpleGrid
+                cols={2}
+                spacing="xl"
+                verticalSpacing="xl"
+                breakpoints={[{ maxWidth: 1348, cols: 1 }]}
+            >
+                <Box>
+                    <Title order={2} mb="md" fw={300}>
+                        Your Mood Over the Last Week
+                    </Title>
+                    <MoodChart />
+                </Box>
+                <Box>
+                    <Group position="apart">
+                        <Title order={2} mt="md" mb="xl" fw={300}>
+                            Your Recent Journal Entries
+                        </Title>
+                        <Button component={Link} variant="outline" to="/journal">
+                            See all →
+                        </Button>
+                    </Group>
+                    <JournalEntries />
+                </Box>
+            </SimpleGrid>
+            <Title order={2} mt="xl" mb="xl" fw={300}>
+                Chat History
+            </Title>
+            <Center>
+                <Button></Button>
+            </Center>
         </>
     );
 };
+
+const JournalEntries = () => {
+    const { classes } = useStyles();
+
+    const entires = useQuery({
+        queryKey: ["JournalEntries"],
+        queryFn: () => {
+            const journalEntries = fetcher.path("/Journals").method("get").create();
+            return journalEntries({});
+        },
+    });
+
+    if (entires.isLoading) return <FullScreenLoading />;
+
+    if (entires.isError)
+        return (
+            <Center className={classes.errorContainer} m="xs">
+                <Title color="red" align="center">
+                    Failed to load journal entries
+                </Title>
+                <Text color="red" align="center">
+                    {" "}
+                    Please try again later or try logging out and the back in.
+                </Text>
+            </Center>
+        );
+
+    return (
+        <div>
+            <SimpleGrid
+                spacing="xs"
+                verticalSpacing="xs"
+                mb="lg"
+                breakpoints={[
+                    { minWidth: "sm", cols: 2 },
+                    { minWidth: "md", cols: 3 },
+                    { minWidth: "lg", cols: 4 },
+                ]}
+            >
+                {entires.data.data
+                    .sort((a, b) => Number.parseInt(b.updatedAt) - Number.parseInt(a.updatedAt))
+                    .slice(0, 8)
+                    .map((entry) => (
+                        <HomeScreenJournalEntry
+                            key={entry.id}
+                            id={entry.id}
+                            title={entry.title}
+                            updatedAt={entry.updatedAt}
+                        ></HomeScreenJournalEntry>
+                    ))}
+            </SimpleGrid>
+        </div>
+    );
+};
+
+const HomeScreenJournalEntry = (props: { id: number; title: string; updatedAt: string }) => {
+    return (
+        <>
+            <Card p="xl" radius="md" withBorder component="a" href={"/journal/" + props.id}>
+                <Text fz="xl" truncate>
+                    {props.title}
+                </Text>
+                <Text color="dimmed" size="xs">
+                    Last edited:
+                </Text>
+                <Text size="sm">{dayjs(props.updatedAt).format("DD MMM YYYY")}</Text>
+            </Card>
+        </>
+    );
+}
 
 export default Home;
